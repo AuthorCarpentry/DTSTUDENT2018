4bffc35a7dd23e6586d46b943ff81d54db88fd48
Tom Morrell <tmorrell@caltech.edu>
2018-08-03 13:50:06 -0700

    Add binder stuff

---
+++ binder.md
@@ -0,0 +1,1 @@
+[![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/AuthorCarpentry/R-Binder/master?urlpath=rstudio)
---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("rmarkdown")
+install.packages("bitops")
+install.packages("caTools")
+install.packages("tidyverse")
+install.packages("DT")
